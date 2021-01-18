9701c1e90bebd52069a594c3a18fb51bb1e5858a
Alireza Mousavi <76426674+AlirezaMsv@users.noreply.github.com>
2021-01-18 11:30:30 +0330

    Update Main.java

--- Codes/Main.java
+++ Codes/Main.java
@@ -1,8 +1,81 @@
 package DSProject;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDragEnabled(true);
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int choice = chooser.showOpenDialog(null);
+        if (choice != JFileChooser.APPROVE_OPTION) {
+            System.out.println("File not selected");
+        }
+        JFrame frameMain = new JFrame();
+        JButton people = new JButton("People");
+        JButton accounts = new JButton("Accounts");
+        JButton calls = new JButton("Calls");
+        JButton cars = new JButton("Cars");
+        JButton homes = new JButton("Homes");
+        JButton ownerships = new JButton("Ownerships");
+        JButton phones = new JButton("Phones");
+        JButton relationships = new JButton("Relationships");
+        JButton transactions = new JButton("Transactions");
+        JPanel panel = new JPanel(new GridLayout(3 , 3));
+        panel.add(people);
+        panel.add(accounts);
+        panel.add(calls);
+        panel.add(cars);
+        panel.add(homes);
+        panel.add(ownerships);
+        panel.add(phones);
+        panel.add(relationships);
+        panel.add(transactions);
+        frameMain.add(panel);
+        frameMain.setLocationRelativeTo(null);
+        frameMain.setSize(400 , 200);
+        frameMain.setVisible(true);
+        frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        people.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                File file = new File(chooser.getSelectedFile().getAbsolutePath()+"\\people.csv");
+                Scanner sc = null;
+                try {
+                    sc = new Scanner(new FileInputStream(file));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ArrayList<String[]> values = new ArrayList<>();
+                while (sc.hasNextLine())
+                {
+                    String line = sc.nextLine();
+                    line = line.substring(1 , line.length()-1);
+                    String[] vals = line.split("\",\"");
+                    values.add(vals);
+                }
+                String[][] strings = new String[values.size()-1][6];
+                String[] raw = values.get(0);
+                for (int i=1;i<values.size();i++)
+                    strings[i-1] = values.get(i);
+                JFrame jFrame = new JFrame();
+                JTable table = new JTable(strings, raw);
+                JScrollPane scrollPane = new JScrollPane(table);
+                jFrame.add(scrollPane);
+                jFrame.setLocationRelativeTo(null);
+                jFrame.setExtendedState( jFrame.getExtendedState()|JFrame.MAXIMIZED_BOTH );
+                jFrame.setVisible(true);
+                jFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            }
+        });
     }
 }
 class Node
